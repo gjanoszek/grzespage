26f32134ca9d93852ed2c098476bffa1ce944c39
Wojciech Janoszek <wjanoszek@icloud.com>
2020-05-12 11:49:51 +0200

    Fix hamburger menu and refactor its logic

--- assets/scripts/hamburger.js
+++ assets/scripts/hamburger.js
@@ -1,51 +1,17 @@
-const hamburger = document.querySelector('.hamburger__button');
-const nav = document.querySelector('.hamburger__drawer');
-const hamburgerActiv = document.querySelector('.hamburger__button--active');
-const navActiv = document.querySelector('.hamburger__drawer--active');
+const hamburgerButton = document.querySelector('.hamburger__button');
+const drawer = document.querySelector('.hamburger__drawer');
 
+const toggleHamburger = () => {
+  hamburgerButton.classList.toggle('hamburger__button--active');
+  drawer.classList.toggle('hamburger__drawer--active');
+};
 
-
-const handleClick = () => {
-  hamburger.classList.toggle('hamburger__button--active');
-  nav.classList.toggle('hamburger__drawer--active');
-}
-
-hamburger.addEventListener('click', handleClick);
-
-
-
-
-const removeClass = () => {
-  if (hamburger.classList.contains('hamburger__drawer--active')) {
-  hamburger.classList.remove('hamburger__button--active');
-  nav.classList.remove('hamburger__drawer--active');
+const hideDrawer = () => {
+  if (drawer.classList.contains('hamburger__drawer--active') && event.target !== hamburgerButton) {
+    hamburgerButton.classList.remove('hamburger__button--active');
+    drawer.classList.remove('hamburger__drawer--active');
   }
-}
+};
 
-document.addEventListener('click', removeClass);
-
-
-
-
-
-
-
-
-
-// const removeClass = () => {
-//   hamburger.classList.remove('hamburger__button--active');
-//   nav.classList.remove('hamburger__drawer--active');
-// }
-
-// if (hamburger.classList.contains('hamburger__button--active')) {
-//   document.addEventListener('click', removeClass);
-// }
-
-
-// const removeClass = () => {
-//   hamburger.classList.remove('hamburger__button--active');
-//   nav.classList.remove('hamburger__drawer--active');
-
-// document.addEventListener('click', () => {
-//   nav.display = 'none';
-// })
+hamburgerButton.addEventListener('click', toggleHamburger);
+document.addEventListener('click', hideDrawer);
