30b7789faa864d1badd6b920f72625784caacbf0
Wojciech Janoszek <wjanoszek@icloud.com>
2020-05-12 18:41:12 +0200

    fix triggering popup while hamburger is open

--- assets/scripts/imagePopup.js
+++ assets/scripts/imagePopup.js
@@ -1,20 +1,15 @@
 const images = document.getElementsByClassName('portfolio__image');
 const imgPopup = document.getElementById('image-popup');
-const drawerActive = document.querySelector('.hamburger__drawer--active');
 
 Array.from(images).forEach(img => {
   img.addEventListener('click', () => {
-    if (!drawerActive) {
-    imgPopup.querySelector('img').src = img.querySelector('img').src;
-    imgPopup.style.display = 'flex';
+    if (!drawer.classList.contains('hamburger__drawer--active')) {
+      imgPopup.querySelector('img').src = img.querySelector('img').src;
+      imgPopup.style.display = 'flex';
     }
   })
 });
 
 imgPopup.addEventListener('click', () => {
   imgPopup.style.display = 'none';
-})
-
-
-
-// const imgPopup = document.getElementById('image-popup');
+});
