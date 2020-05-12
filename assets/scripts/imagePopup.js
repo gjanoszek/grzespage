9577c4aff3780bbf3fe9a3b2f689fc11ccb17bf0
Grzesiek <grzechu.noname@gmail.com>
2020-05-12 17:59:00 +0200

    drawer bug

--- assets/scripts/imagePopup.js
+++ assets/scripts/imagePopup.js
@@ -1,13 +1,20 @@
 const images = document.getElementsByClassName('portfolio__image');
 const imgPopup = document.getElementById('image-popup');
+const drawerActive = document.querySelector('.hamburger__drawer--active');
 
 Array.from(images).forEach(img => {
   img.addEventListener('click', () => {
+    if (!drawerActive) {
     imgPopup.querySelector('img').src = img.querySelector('img').src;
     imgPopup.style.display = 'flex';
+    }
   })
 });
 
 imgPopup.addEventListener('click', () => {
   imgPopup.style.display = 'none';
 })
+
+
+
+// const imgPopup = document.getElementById('image-popup');
