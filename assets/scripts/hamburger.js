5e3e66260d3d39ed9c0283017d2f0b887821245a
Grzesiek <grzechu.noname@gmail.com>
2020-05-12 11:16:06 +0200

    hamburger deactivation

--- assets/scripts/hamburger.js
+++ assets/scripts/hamburger.js
@@ -1,11 +1,51 @@
 const hamburger = document.querySelector('.hamburger__button');
 const nav = document.querySelector('.hamburger__drawer');
+const hamburgerActiv = document.querySelector('.hamburger__button--active');
+const navActiv = document.querySelector('.hamburger__drawer--active');
+
 
 
 const handleClick = () => {
   hamburger.classList.toggle('hamburger__button--active');
   nav.classList.toggle('hamburger__drawer--active');
-
 }
 
 hamburger.addEventListener('click', handleClick);
+
+
+
+
+const removeClass = () => {
+  if (hamburger.classList.contains('hamburger__drawer--active')) {
+  hamburger.classList.remove('hamburger__button--active');
+  nav.classList.remove('hamburger__drawer--active');
+  }
+}
+
+document.addEventListener('click', removeClass);
+
+
+
+
+
+
+
+
+
+// const removeClass = () => {
+//   hamburger.classList.remove('hamburger__button--active');
+//   nav.classList.remove('hamburger__drawer--active');
+// }
+
+// if (hamburger.classList.contains('hamburger__button--active')) {
+//   document.addEventListener('click', removeClass);
+// }
+
+
+// const removeClass = () => {
+//   hamburger.classList.remove('hamburger__button--active');
+//   nav.classList.remove('hamburger__drawer--active');
+
+// document.addEventListener('click', () => {
+//   nav.display = 'none';
+// })
